d58afbc9c5872f7c3c95d1cb8514146e92c4915d
Paul Vaughan <hello@mywaytaxis.co.uk>
2026-03-12 23:12:51 +0000

    add useEffect import in edit page

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getSupabase } from "../../lib/supabase";
 import { useParams, useRouter } from "next/navigation";
 
